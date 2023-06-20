912b228a6ea9c00abe73bea4643c3180c39b7028
Gustavo <you@example.com>
2023-06-20 20:03:28 -0300

    client -> server aleatório

--- src/br/org/catolicasc/client/GreetClient.java
+++ src/br/org/catolicasc/client/GreetClient.java
@@ -1,10 +1,10 @@
 package br.org.catolicasc.client;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class GreetClient {
     private Socket clientSocket;
@@ -13,34 +13,50 @@ public class GreetClient {
 
     public void start(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
-        // handler para escrita de dados
+        // handler para escrever os dados
         out = new PrintWriter(clientSocket.getOutputStream(), true);
-        // handler para leitura de dados
+        // handler para ler os dados
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
     }
 
-    public void stop(){
+    public void stop() {
         try {
             in.close();
             out.close();
             clientSocket.close();
         } catch (IOException ex) {
-            System.out.println("Erro ao fechar a conexão");
+            System.out.println("Erro ao fechar a conexão 1.");
         }
     }
 
-    public String sendMessage(String msg) throws IOException {
-        out.println(msg); // manda msg para o socket
-        return in.readLine(); // retorna mensagem recebida
+    public void sendMessage(String msg) throws IOException {
+        out.println(msg); // envia mensagem para o Socket
+    }
+
+    public String receiveMessage() throws IOException {
+        return in.readLine(); // retorna a mensagem recebida do Socket
     }
 
     public static void main(String[] args) {
         GreetClient client = new GreetClient();
         try {
             client.start("127.0.0.1", 12345);
-            String response = client.sendMessage("Hello server");
-            System.out.println("Resposta do Servidor: " + response);
+
+            Scanner scanner = new Scanner(System.in);
+            String jogada;
+            String response;
+
+            // Game logic
+            do {
+                System.out.print("Escolha um número de 1 a 10: ");
+                jogada = scanner.nextLine();
+                client.sendMessage(jogada);
+                response = client.receiveMessage();
+                System.out.println("Resposta: " + response);
+            } while (!"!quit".equals(jogada) && !"Acertou!".equals(response));
+
+            System.out.println("Fechando o jogo...");
+
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         } finally {
@@ -48,4 +64,3 @@ public class GreetClient {
         }
     }
 }
-
--- src/br/org/catolicasc/server/GreetServer.java
+++ src/br/org/catolicasc/server/GreetServer.java
@@ -1,38 +1,57 @@
 package br.org.catolicasc.server;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.ClientInfoStatus;
+import java.util.Random;
 
 public class GreetServer {
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
+    private int chosenNumber;
 
     public void start(int port) throws IOException {
-        //inicializar atributos
-        serverSocket = new ServerSocket(port); // escuta na porta port
-        clientSocket = serverSocket.accept();  // espera a conexão
-        // handler para escrita de dados
+        // Initialize attributes
+        serverSocket = new ServerSocket(port); // listen on the specified port
+        clientSocket = serverSocket.accept(); // wait for connection
+        // handler for writing data
         out = new PrintWriter(clientSocket.getOutputStream(), true);
-        // handler para leitura de dados
+        // handler for reading data
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//        chosenNumber = numeroRandom();
+//        System.out.println("Numero Escolhido: " + chosenNumber); // Print the chosen number
 
         clientHandler();
     }
 
     private void clientHandler() throws IOException {
-        String greeting = in.readLine();
-        if("Hello server".equals(greeting)){
-            out.println("Hello client");
-        } else {
-            out.println("Mensagem Incorreta.");
+        String jogada;
+        //out.println("Escolhendo um número de 1 a 10");
+
+        while (true) {
+            chosenNumber = numeroRandom();
+            System.out.println("Numero Escolhido: " + chosenNumber);
+            jogada = in.readLine();
+            if (jogada.equals("!quit")) {
+                out.println("Saindo do jogo");
+                break;
+            }
+//            out.println("Escolhendo um número de 1 a 10");
+
+            int guessedNumber = Integer.parseInt(jogada);
+            if (guessedNumber == chosenNumber) {
+                out.println("Acertou!");
+                break;
+            } else {
+                out.println("Errou!");
+            }
         }
+
+        System.out.println("Fechando conexão");
     }
 
     public void stop() {
@@ -42,8 +61,13 @@ public class GreetServer {
             clientSocket.close();
             serverSocket.close();
         } catch (IOException ex) {
-            System.out.println("Erro ao fechar a conexão");
+            System.out.println("Erro ao fechar a conexão!");
         }
+    }
+
+    private int numeroRandom() {
+        Random random = new Random();
+        return random.nextInt(10) + 1;
     }
 
     public static void main(String[] args) {
@@ -55,6 +79,5 @@ public class GreetServer {
         } finally {
             server.stop();
         }
-
     }
 }
